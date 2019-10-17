59a0f71f99670f0e4ddc5ac2ec91075a806bea64
Eyal.Kapah <eykapah@microsoft.com>
2019-10-17 14:26:04 +0300

    Add project files.

---
+++ Euro.API/Configurations/ConfigureConnections.cs
@@ -0,0 +1,21 @@
+﻿using Euro.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Euro.API.Configurations
+{
+    public static class ConfigureConnections
+    {
+        public static IServiceCollection AddConnectionProvider(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connection = string.Empty;
+
+            connection = configuration.GetConnectionString("EuroDb");
+
+            services.AddDbContext<EuroContext>(services => services.UseSqlServer(connection));
+
+            return services;
+        }
+    }
+}
---
+++ Euro.Context/EuroContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Euro.Context
+{
+    public class EuroContext : DbContext
+    {
+        public EuroContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
