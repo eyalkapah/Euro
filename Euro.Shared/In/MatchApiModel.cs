84b194177891675c11c3d071cee4bae8a9fa3930
Eyal Kapah <kaycee1@gmail.com>
2019-12-26 10:24:26 +0200

    fix match api

--- Euro.Shared/In/MatchApiModel.cs
+++ Euro.Shared/In/MatchApiModel.cs
@@ -1,17 +1,26 @@
 ﻿using System;
+using System.Text.Json.Serialization;
 
 namespace Euro.Domain.ApiModels
 {
     public class MatchApiModel
     {
+        [JsonPropertyName("group_id")]
+        public int GroupId { get; set; }
+
+        [JsonPropertyName("guest_scored")]
         public int GuestScored { get; set; }
 
+        [JsonPropertyName("guest_team_id")]
         public int GuestTeamId { get; set; }
 
+        [JsonPropertyName("host_scored")]
         public int HostScored { get; set; }
 
+        [JsonPropertyName("host_team_id")]
         public int HostTeamId { get; set; }
 
+        [JsonPropertyName("play_date_time")]
         public DateTime PlayDateTime { get; set; }
     }
 }
--- Euro.Shared/Out/MatchResultApiModel.cs
+++ Euro.Shared/Out/MatchResultApiModel.cs
@@ -1,22 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.Json.Serialization;
 
 namespace Euro.Shared.Out
 {
     public class MatchResultApiModel
     {
+        [JsonPropertyName("group_id")]
+        public int GroupId { get; set; }
+
+        [JsonPropertyName("guest_scored")]
         public int GuestScored { get; set; }
 
+        [JsonPropertyName("guest_team_id")]
         public int GuestTeamId { get; set; }
 
+        [JsonPropertyName("host_scored")]
         public int HostScored { get; set; }
 
+        [JsonPropertyName("host_team_id")]
         public int HostTeamId { get; set; }
+
+        [JsonPropertyName("match_id")]
         public int MatchId { get; set; }
 
+        [JsonPropertyName("play_date_time")]
         public DateTime PlayDateTime { get; set; }
     }
 }
